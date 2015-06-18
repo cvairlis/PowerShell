c8136a742965823a39f46b1d6f6cc5a98ca35564
greekIT <greekit@users.noreply.github.com>
2015-06-18 23:25:58 +0300

    Job Scheduler some comments added

--- Modules/LogAnalysis/JobScheduler.ps1
+++ Modules/LogAnalysis/JobScheduler.ps1
@@ -1,13 +1,20 @@
-﻿
+﻿<# This script created in order to create a Scheduled Job for filling the DataBase every 10 minutes.
+   Actually every 10 minutes calls ScheduleLogs.ps1 and this is doing the job.
+   When you will run the script you will be prompt for credential. 
+   After this you can go to Task Scheduler from Administrative Tools to check if the job appears there.  #>
+
+
 # Change these three variables to whatever you want
 $jobname = "Automate Log Database Filling"
+
+# Here is where your ScheduleLogs.ps1 script exists.
 $script =  "C:\Users\Administrador\Documents\WindowsPowerShell\Modules\LogAnalysis\ScheduleLogs.ps1"
 $repeat = (New-TimeSpan -Minutes 10)
  
 
 # The script below will run as the specified user (you will be prompted for credentials)
 # and is set to be elevated to use the highest privileges.
-# In addition, the task will run every 5 minutes or however long specified in $repeat.
+# In addition, the task will run every 10 minutes or however long specified in $repeat.
 $scriptblock = [scriptblock]::Create($script)
 #$trigger = New-JobTrigger -AtStartup -RepeatIndefinitely -RepetitionInterval $repeat
 $trigger = New-JobTrigger -Once -At (Get-Date).Date -RepeatIndefinitely -RepetitionInterval $repeat
