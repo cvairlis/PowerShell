71fc8bac77497fde7adb8e44721f43e16eb58ac0
greekIT <greekit@users.noreply.github.com>
2015-07-21 18:08:08 +0300

    script for  getting system information on powershell console

---
+++ System_Information_Tool/Modules/SystemInformation/SystemInformation.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host "`nSystem Information Summary`n"
+[string]$OS = (Get-WmiObject -Class Win32_OperatingSystem).Caption
+$OS += " " + (Get-WmiObject -Class Win32_OperatingSystem).OSArchitecture
+
+
+Write-Host "Operating System: $OS"
+
+[string]$CPU = (Get-WmiObject -Class Win32_Processor).Name
+#$CPU +=
+
+Write-Host "CPU: $CPU"
+
+[System.UInt64]$Memory = (Get-WMIObject -class Win32_PhysicalMemory).Capacity
+[string]$MemoryToString = ([math]::Round($Memory/1GB,2)).ToString()
+$MemoryToString += " GB"
+
+Write-Host "Memory: $MemoryToString"
