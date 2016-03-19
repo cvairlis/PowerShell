39c37310caca054d5294baa2d30bf43652537c20
Charis <h.vairlis@gmail.com>
2016-03-19 16:47:22 +0200

    hids powershell version 1

---
+++ PowerShellHIDS/version1/Modules/LogDatabase/LogDatabase.psm1
@@ -0,0 +1,145 @@
+﻿<#
+.NAME
+   Get-LogDatabaseData
+
+.Synopsis
+   Queries information from the database.
+ 
+.DESCRIPTION
+    Get-LogDatabaseData is to be used when you want to query information from the database.
+    
+.PARAMETERS
+    -ConnectionString<String>
+        Tells PowerShell how to find the database server, what database to connect to, and how to authenticate.
+        You can find more connection string examples at: "http://connectionstrings.com"
+
+        Required?                    false
+        Position?                    named
+        Default value                Local computer
+        Accept pipeline input?       True (ByPropertyName)
+        Accept wildcard characters?  false
+
+    -isSQLServer<Switch>
+        Include this switch when your connection string points to a Microsoft SQL Server.
+        Omit this string for all other database server types, and PowerShell will use
+        OleDB instead. You'll need to make sure your connection string is OleDB compatible
+        and that you're installed the necessary OleDB drivers to access your databse.
+        That can be MySQL, Access, Oracle, or whatever you like.
+
+    -Query<String>
+        This is the actual SQL language query that you want to run. 
+        This module isn't going to dive into detail on that language; we assume you know it already.
+        If you'd like to learn more about the SQL language, there are numerous books and videos on the subject.
+.NOTES
+    Get-LogDatabaseData will retrieve data and place it into the pipeline.
+    Within the pipeline, you'get objects with properties that correspond to the columns of the database.
+    We're not going to dive into further detail on how the two database functions:
+    (Get-LogDatabaseData & Invoke-LogDatabaseQuery) operate internally.
+    These functions internally utilize the .NET Frameworkm and so for this module they're out of scope.
+    The functions do, however, provide a nice wrapper around .NET, so that you can access databases
+    without having to mess around with the raw .NET Framework stuff.
+
+#>
+function Get-LogDatabaseData
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+       [String]$connectionString,
+       [String]$query,
+       [switch]$isSQLServer
+    )
+    if ($isSQLServer) {
+        Write-Verbose 'in SQL Server mode'
+        $connection = New-Object -TypeName `
+            System.Data.SqlClient.SqlConnection    
+    } else {
+        Write-Verbose 'in OleDB mode'
+        $connection = New-Object -TypeName `
+            System.Data.OleDb.OleDbConnection
+    }
+
+    $connection.ConnectionString = $connectionString
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+   
+    if ($isSQLServer) {
+        $adapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter $command
+    } else {
+        $adapter = New-Object -TypeName System.Data.OleDb.OleDbDataAdapter $command
+    }
+
+    $dataset = New-Object -TypeName System.Data.DataSet
+    #I put in var a to prevent to return an int value
+    $a = $adapter.Fill($dataset)
+    $connection.Close()
+}
+
+<#
+.Name 
+   Invoke-LogDatabaseQuery
+
+.Synopsis
+   Make changes on the database.
+
+.DESCRIPTION
+   Invoke-LogDatabaseQuery is for when you want to make changes on the database.
+   You can add, remove or change data.
+
+.PARAMETERS
+    -ConnectionString<String>
+        Tells PowerShell how to find the database server, what database to connect to, and how to authenticate.
+        You can find more connection string examples at: "http://connectionstrings.com"
+
+        Required?                    false
+        Position?                    named
+        Default value                Local computer
+        Accept pipeline input?       True (ByPropertyName)
+        Accept wildcard characters?  false
+
+    -isSQLServer<Switch>
+        Include this switch when your connection string points to a Microsoft SQL Server.
+        Omit this string for all other database server types, and PowerShell will use
+        OleDB instead. You'll need to make sure your connection string is OleDB compatible
+        and that you're installed the necessary OleDB drivers to access your databse.
+        That can be MySQL, Access, Oracle, or whatever you like.
+
+    -Query<String>
+        This is the actual SQL language query that you want to run. 
+        This module isn't going to dive into detail on that language; we assume you know it already.
+        If you'd like to learn more about the SQL language, there are numerous books and videos on the subject.
+
+.NOTES
+    Invoke-LogDatabaseQuery doesn't write anything to the pipeline; it just runs your query.
+    It also declares support for the -WhatIf and -Confirm parameterns via its SupportsShouldProcess attribute.
+
+#>
+function Invoke-LogDatabaseQuery
+{
+    [CmdletBinding(SupportsShouldProcess=$true,
+                   ConfirmImpact='Low')]    
+    Param
+    (
+        [string]$connectionString,
+        [string]$query,
+        [switch]$isSQLServer
+    )
+    if ($isSQLServer) {
+        Write-Verbose 'in SQL Server mode'
+        $connection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+    } else {
+        Write-Verbose 'in OleDB mode'
+        $connection = New-Object -TypeName System.Data.OleDb.OleDbConnection
+    }
+    $connection.ConnectionString = $connectionString
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+    if ($pscmdlet.shouldprocess($query)) {
+        $connection.Open()
+        Write-Verbose $query
+        # ExecuteNonQuery: Executes a Transact-SQL statement against the connection and returns the number of rows affected.
+        $returnValue = $command.ExecuteNonQuery()
+        $connection.close()
+    }
+}
---
+++ PowerShellHIDS/version1/README.md
@@ -0,0 +1,73 @@
+# PowerShell
+PowerShell stuff !!!
+
+LOG README v1.0
+
+Windows Event Log Analysis Visualization & Reporting using only one tool: PowerShell.
+
+You need of course to download and install Microsoft SQL Server 2012 Expert 
+download link below to choose version:
+https://www.microsoft.com/en-us/download/details.aspx?id=29062
+
+(or for the x64 ENU version you can use the following link:
+http://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SQLEXPRWT_x64_ENU.exe)
+
+
+This readme will be updated in order to give clear instructions to download install and use this module.
+
+It has been implemented for academic purpoces of University of Macedonia Thessaloniki Greece (Oct 2014-June 2015). 
+Particularly it is part of my undergraduate thesis that had Information Security and System Administration concerns.
+
+It is not finished yet. It will be updated with full documentation and comments for every cmdlet and whatever it needs.
+
+
+WAY IT WORKS:
+
+- Creating DataBase
+  New DataBase
+  Name LogDB
+  Create a new Table Events (I give the sql code for this)
+
+- Create New Folder for PowerShell Modules
+  Save Modules at C:\Users\"currentuser"\Documents\WindowsPowerShell\Modules
+
+- Saving Events in DataBase
+  Open Windows PowerShell and type Import-Module LogAnalysis and then type the following:
+  PS C:\> Get-WinEvent -LogName Application, Security, System | Sort-Object -Property TimeCreated | Set-LogEventInDatabase
+
+- Auto Created Tables
+  DETAILS 4625
+  DETAILS 4624 
+  etc
+
+- Schedule Automatic DataBase filling (JobScheduler.ps1)
+
+- Convert (LogVisualization.ps1) to executable so you can run it with double click.
+  https://gallery.technet.microsoft.com/PS2EXE-Convert-PowerShell-9e4e07f1
+
+- Log Visualization (LogVisualization.ps1) goes like this:
+  A window appears and you can choose between History, Intraday and Custom Range visualization. 
+     It automatically finds which is the last event stored date and:
+     - For the last 7 days it visualizes events per Hour.
+     - For dates more than 7 days it visualizes events per Day.
+     - For dates more than 30 days it visualizes events per Week.
+
+  You select time range you are interest for and you can view:
+  - Pie Charts
+  - Time Line Charts
+  - Events Occured Number for every Event Log or for All events
+  - Events Occured By Groups (for all type of view)
+  - RadioButtons to view whatever you want
+  - Save current chart (folder automatically is been created in the path that the script or .exe runs)
+  
+
+
+KEY POINTS:
+
+- Dates Time and TimeRanges American Style
+
+- DataBase have to re-created every new Year to contain only events occured the current year !!! 
+	In other words the first event that is stored in the database have to be after 01/04/20xx.
+	This is because if there are events from past years log analysis - Get-HashTableForTimeLineChart cmdlet cannot produce correct results.
+
+
